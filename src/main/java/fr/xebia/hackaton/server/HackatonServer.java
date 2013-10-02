639b740789d09deb0078c440ff29771dfaa316fc
Gerome Egron <legunda.gg&gmail.com>
2013-10-02 21:50:24 +0200

    First commit

---
+++ src/main/java/fr/xebia/hackaton/server/HackatonServer.java
@@ -0,0 +1,24 @@
+package fr.xebia.hackaton.server;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static spark.Spark.get;
+import static spark.Spark.setPort;
+
+public class HackatonServer {
+    private static final String PORT = System.getenv("PORT");
+
+    public static void main(String[] args) {
+        setPort(new Integer(PORT));
+
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World!";
+            }
+        });
+    }
+
+}
