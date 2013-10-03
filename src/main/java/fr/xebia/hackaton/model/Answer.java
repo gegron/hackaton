22ad78144e7f7751640eed8f15ececf278a897f0
Gerome Egron <legunda.gg&gmail.com>
2013-10-03 19:50:00 +0200

    new test

--- src/main/java/fr/xebia/hackaton/model/Answer.java
+++ src/main/java/fr/xebia/hackaton/model/Answer.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Answer {
 
     static Map<String, String> answers = new HashMap<String, String>() {{
-        put("Quelle est ton Identity ? (cf dashboard)", "Gérôme Egron");
+        put("Quelle est ton Identity ? (cf dashboard)", "Gerome EGRON");
     }};
 
     public static String findAnswer(String question) {
