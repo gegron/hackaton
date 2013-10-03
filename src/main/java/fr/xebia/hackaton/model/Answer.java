9aa625a56600569beb7af7618c09e2734b142038
Gerome Egron <legunda.gg&gmail.com>
2013-10-03 21:07:18 +0200

    last answer

--- src/main/java/fr/xebia/hackaton/model/Answer.java
+++ src/main/java/fr/xebia/hackaton/model/Answer.java
@@ -1,13 +1,22 @@
 package fr.xebia.hackaton.model;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -89,10 +98,35 @@ public class Answer {
         }
 
         if (urlMatcher.find()) {
-            String url = urlMatcher.group(1);
+            String urlStrint = urlMatcher.group(1);
 
-//            Request.Get(url)
-//                    .execute().returnContent();
+
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(urlStrint);
+
+            // add request header
+            HttpResponse response = null;
+            try {
+                response = client.execute(request);
+
+
+                System.out.println("Response Code : "
+                        + response.getStatusLine().getStatusCode());
+
+                BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+                StringBuffer result = new StringBuffer();
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+
+                return result.toString();
+            }
+            catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
         }
 
         return answers.get(question);
