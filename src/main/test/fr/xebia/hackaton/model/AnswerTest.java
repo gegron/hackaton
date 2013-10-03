4749750f8b729507341261b8b0bf1e79e6dd73f1
Jean-Eudes <jeudes7@gmail.com>
2013-10-03 19:55:29 +0200

    add test

---
+++ src/main/test/fr/xebia/hackaton/model/AnswerTest.java
@@ -0,0 +1,17 @@
+package fr.xebia.hackaton.model;
+
+
+import org.fest.assertions.Assertions;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class AnswerTest {
+
+    @Test
+    public void should_answer_question1() throws Exception {
+        String answer = Answer.findAnswer("Quelle est ton Identity ? (cf dashboard)");
+        assertThat(answer).isEqualTo("Gerome EGRON");
+
+    }
+}
